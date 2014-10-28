714169a23c0eca7ca9aca026f341da595fefda3e
mazybear <zanizone45@gmail.com>
2014-10-28 19:41:01 -0400

    Lowercase 'd'

--- lights_ms2.py
+++ lights_ms2.py
@@ -7,7 +7,7 @@ def set_lights(data):
     propagate = json_obj['propagate']
     led.fill(Color(0, 0, 0, 0), start=0, end=31)
     for light in lights:
-        light_id = light['lightID'] - 1
+        light_id = light['lightId'] - 1
         if propagate:
             led.fill(Color(light['red'], light['green'], light['blue'], light['intensity']), start=light_id, end=31)
         else:
