20c6b4450deb1b0e2fcfadec707af46f20b93144
Matt Irvine <mji7wb@virginia.edu>
2014-10-28 18:29:31 -0400

    Fixing bug where it always gave local ip on the rpi

--- startup.py
+++ startup.py
@@ -1,7 +1,10 @@
 import urllib2, socket, sys, time
-ip = socket.gethostbyname(socket.getfqdn())
 while True:
 	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(("gmail.com", 80))
+		ip = s.getsockname()[0]
+		s.close()
 		urllib2.urlopen("http://cs4720.cs.virginia.edu/ipregistration/?pokemon=Natu&ip=" + ip)
 		sys.exit(0)
 	except urllib2.URLError:
