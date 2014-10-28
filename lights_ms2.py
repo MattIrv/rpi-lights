1fe72ce8040bf9154a6c70d2cec3e6837edeed02
mazybear <zanizone45@gmail.com>
2014-10-28 19:13:36 -0400

    Fixed intensity values

--- lights_ms2.py
+++ lights_ms2.py
@@ -6,11 +6,10 @@ def set_lights(data):
     lights = json_obj['lights']
     propagate = json_obj['propagate']
     led.masterBrightness = 0.0
-    led.fill((0, 0, 0), start=0, end=31)
+    led.fill(Color(0, 0, 0, 0), start=0, end=31)
     for light in lights:
         light_id = light['lightID'] - 1
-        led.masterBrightness = light['intensity']
         if propagate:
-            led.fill((light['red'], light['green'], light['blue']), start=light_id, end=31)
+            led.fill(Color(light['red'], light['green'], light['blue'], light['intensity']), start=light_id, end=31)
         else:
-            led.fill((light['red'], light['green'], light['blue']), start=light_id, end=light_id)
+            led.fill(Color(light['red'], light['green'], light['blue'], light['intensity']), start=light_id, end=light_id)
