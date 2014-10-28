b87ee2224d2b4928083739552acfa313903aa336
mazybear <zanizone45@gmail.com>
2014-10-28 19:17:42 -0400

    No more master brightness

--- lights_ms2.py
+++ lights_ms2.py
@@ -5,7 +5,6 @@ def set_lights(data):
     json_obj = json.dumps(data)
     lights = json_obj['lights']
     propagate = json_obj['propagate']
-    led.masterBrightness = 0.0
     led.fill(Color(0, 0, 0, 0), start=0, end=31)
     for light in lights:
         light_id = light['lightID'] - 1
