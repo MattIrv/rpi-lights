b284532a8e05ad87ee35b52b0d2275f83453cd29
dbm3tz <dbm3tz@virginia.edu>
2014-11-09 18:37:05 -0500

    Added first pages code.

---
+++ FirstPages/Settings.java
@@ -0,0 +1,42 @@
+package com.example.student.mkyongtutorial;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Settings extends Activity {
+
+    Button button5;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+        addListenerOnButton5();
+    }
+
+    public void addListenerOnButton5() {
+
+        final Context context = this;
+
+        button5 = (Button) findViewById(R.id.button5);
+
+        button5.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, AppActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+
+}
